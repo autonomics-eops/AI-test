7f95648643fadf74370bfbb1b31eeb4786ba46cf
autonomics-eops <AUTONOMICS-DEVOPS@hcltech.com>
2025-12-02 16:28:33 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "subscription_id" {}
+variable "tenant_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "location" {
+  default = "eastus"
+}
+variable "rg_name" {
+  default = "rg-ai-landingzone"
+}
+variable "vnet_name" {
+  default = "vnet-ai"
+}
+variable "address_space" {
+  default = ["10.10.0.0/16"]
+}
+variable "storage_name" {
+  default = "aistoragelz123"
+}
+variable "aio_name" {
+  default = "ai-foundry-demo"
+}
