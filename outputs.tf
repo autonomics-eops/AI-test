e4f4f07716d8df248b887372cb8b5d305327aa62
autonomics-eops <AUTONOMICS-DEVOPS@hcltech.com>
2025-12-02 19:47:25 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@ output "rg_name" {
   value = azurerm_resource_group.rg.name
 }
 output "ai_foundry_endpoint" {
-  value = azurerm_ai_foundry_workspace.foundry.id
+  value = azapi_resource.ai_foundry.id
 }
