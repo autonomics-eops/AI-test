64b0a194b4d1c35a1d2b3270ca342e4c10c0ab78
autonomics-eops <AUTONOMICS-DEVOPS@hcltech.com>
2025-12-02 16:39:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_subnet" "default" {
+  name                 = "default"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_ai_foundry_workspace" "foundry" {
+  name                = var.aio_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
