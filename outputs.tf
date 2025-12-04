b546721c85fc43f1ff497b6db6efa69ef0deb1bd
autonomics-eops <AUTONOMICS-DEVOPS@hcltech.com>
2025-12-04 17:34:31 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,10 @@ output "rg_name" {
 output "ai_foundry_endpoint" {
   value = azapi_resource.ai_foundry.id
 }
+output "foundry_account_id" {
+  value = azapi_resource.ai_foundry.id
+}
+
+output "foundry_model_deployment_id" {
+  value = azapi_resource.aifoundry_deployment_gpt_4o.id
+}
